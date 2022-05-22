75372477a25c762afefe220a05d5a077992cb691
Estevao Pascoal Joao Dimaca <estevaodimaca@gmail.com>
2022-05-22 11:03:10 +0200

    Criacao dos projectos

---
+++ dimasoft-dsmovie-backend/src/main/java/mz/co/dimasoft/dimasoftdsmoviebackend/DimasoftDsmovieBackendApplication.java
@@ -0,0 +1,13 @@
+package mz.co.dimasoft.dimasoftdsmoviebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimasoftDsmovieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimasoftDsmovieBackendApplication.class, args);
+	}
+
+}
---
+++ dimasoft-dsmovie-backend/src/test/java/mz/co/dimasoft/dimasoftdsmoviebackend/DimasoftDsmovieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.co.dimasoft.dimasoftdsmoviebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimasoftDsmovieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
